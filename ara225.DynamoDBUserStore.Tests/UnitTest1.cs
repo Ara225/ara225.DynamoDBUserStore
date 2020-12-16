994afa48c5f7d532243852053acedee7066353b3
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-12-16 23:14:20 +0000

    Add project files.

---
+++ ara225.DynamoDBUserStore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ara225.DynamoDBUserStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
