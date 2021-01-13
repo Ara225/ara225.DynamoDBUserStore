0516ad84d0cbd6b7cda12f8e90e89eeb67e436b5
Anna Aitchison <anna11199810@hotmail.co.uk>
2021-01-13 01:26:08 +0000

    Implement datetime converter functions

---
+++ ara225.DynamoDBUserStore/DateTimeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace ara225.DynamoDBUserStore
+{
+	public class DateTimeConverter : IPropertyConverter
+	{
+		public DynamoDBEntry ToEntry(object value)
+		{
+			return ((DateTime)value).ToString("o");
+		}
+
+		public object FromEntry(DynamoDBEntry entry)
+		{
+			return DateTime.ParseExact(entry.AsString(), "o", null);
+		}
+	}
+}
---
+++ ara225.DynamoDBUserStore/DateTimeOffsetConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace ara225.DynamoDBUserStore
+{
+	public class DateTimeOffsetConverter : IPropertyConverter
+	{
+		public DynamoDBEntry ToEntry(object value)
+		{
+			return ((DateTimeOffset)value).ToString("o");
+		}
+
+		public object FromEntry(DynamoDBEntry entry)
+		{
+			return DateTimeOffset.ParseExact(entry.AsString(), "o", null);
+		}
+	}
+}
